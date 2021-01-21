820274e14b2e0a87ca1f36738d10e123555fe51b
Fluffy-Lion <leoncooper31@gmail.com>
2021-01-21 14:45:42 +0000

    read me

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+// npm install!
 
-function App() {
+import React, { useState } from 'react'
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom'
+import './App.css';
+import Norris from './components/Norris'
+import Pokemon from './components/Pokemon'
+//information in the provided can be avaibale in the context
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => {
+    return (
+      setCount(count + 1)
+
+    )
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav className="navBar">
+          <Link to="/">home</Link>
+          <Link to="/about">about</Link>
+          <Link to="/contact">contact</Link>
+          <Link to="/pokemon">pokemon</Link>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <h1>hello</h1>
+          </Route>
+          <Route path="/about">
+            <h1>{count}</h1>
+            <button onClick={increaseCount}>plus 1</button>
+          </Route>
+          <Route path="/contact">
+            <h1>contact</h1>
+          </Route>
+          <Route path="/pokemon">
+            <Pokemon/>
+          </Route>
+        </Switch>
+        {/* <div>
+          <h1>hello</h1>
+        </div> */}
+      </Router>
     </div>
   );
 }
+
+// const Norris = () => {
+//   fetch("https://api.chucknorris.io/jokes/random")
+//   .then(res => res.json())
+//   .then(data => console.log(data.value))
+//   // const fact = data.value
+//   return (
+//     <div>
+//       <h1>norris component</h1>
+//       {/* <p>{fact}</p> */}
+//     </div>
+//   )
+// }
 
 export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const Pokemon = () => {
+    const [pokemon, setPokemon] = useState("")
+    const [mainImg, setMainImg] = useState("")
+
+
+   const findDitto = () => {
+    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(response => response.json())
+    .then(data => {
+        setPokemon(data.name)
+        setMainImg(data.sprites.front_default)
+    })
+    }
+return (
+    <div>
+        <h1> pokemon component file</h1>
+        <button onClick={() => findDitto()}>find ditto</button>
+        <h1>{pokemon}</h1>
+        <img src={mainImg} />
+    </div>
+)
+}
+
+export default Pokemon
